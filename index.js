50a6774f78e91c1d311557235a4d238a5347d69a
Maya Borrero <mlbsindia@gmail.com>
2021-09-21 13:32:43 -0400

    commit tree 08767de62105a1c6e5dce7c2f8f732727c5fea8f

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideHeight){
+   if (rideHeight <= 400){
+      return "This one is on me!"}
+    else if (rideHeight <2500) {
+      return "I will gladly take your thirty bucks."}
+    else if (rideHeight >= 2501) {
+      return "No can do."}
+  }
+
+
+function ternaryCheckCity(cityName){
+  return cityName === "NYC" ? ("Ok, sounds good."):("No go.");
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipType){
+  switch (tipType) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    case 'thanks for everything':
+      return 'Bye.'
+  }
 }
